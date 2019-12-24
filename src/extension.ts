aa32ec6d58b5f1b60df59b3907a6956537a0242b
hashrock <hashrock@users.noreply.github.com>
2019-12-24 16:15:50 +0900

    upup

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ function getDiskPath(context: vscode.ExtensionContext, fileName: string) {
 }
 
 let _panel: vscode.WebviewPanel;
+let _editor: vscode.TextEditor;
 
 export function activate(context: vscode.ExtensionContext) {
   const css = getDiskPath(context, "index.css");
@@ -18,31 +19,42 @@ export function activate(context: vscode.ExtensionContext) {
   if (!editor) {
     return "";
   }
-  let working_file_path = editor.document.fileName;
-  vscode.window.onDidChangeVisibleTextEditors(() => {
-    console.log("fire");
-  });
+  _editor = editor;
 
-  // vscode.window.onDidChangeActiveTextEditor(
-  //   editor => {
-  //     _panel.webview.postMessage({
-  //       command: "text",
-  //       text: editor?.document.getText()
-  //     });
-  //   },
-  //   null,
-  //   context.subscriptions
-  // );
-  vscode.workspace.onDidChangeTextDocument(
-    (e: vscode.TextDocumentChangeEvent) => {
+  let working_file_path = _editor.document.fileName;
+  // vscode.window.onDidChangeVisibleTextEditors(() => {
+  //   console.log("fire");
+  // });
+
+  vscode.window.onDidChangeActiveTextEditor(
+    editor => {
+      if(editor === undefined){
+        return;
+      }
+
+      if (editor) {
+        _editor = editor;
+      }
+      _panel.title = editor ? _editor.document.fileName : "No File";
       _panel.webview.postMessage({
         command: "text",
-        text: editor?.document.getText()
+        text: _editor.document.getText()
       });
     },
     null,
     context.subscriptions
   );
+  vscode.workspace.onDidChangeTextDocument(
+    (e: vscode.TextDocumentChangeEvent) => {
+      _panel.webview.postMessage({
+        command: "text",
+        text: _editor.document.getText()
+      });
+    },
+    null,
+    context.subscriptions
+  );
+
 
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.helloWorld", () => {
@@ -59,11 +71,16 @@ export function activate(context: vscode.ExtensionContext) {
       );
       _panel = panel;
       panel.webview.html = getWebviewContent(css, js);
+      
+      _panel.onDidDispose((e) => {
+        console.log("dispose", e)
+        //TODO 対象editorを破棄する必要あり
+      }, undefined, context.subscriptions);
 
       panel.webview.onDidReceiveMessage(message => {
         switch (message.command) {
           case "alert":
-            let textDocument = editor?.document;
+            let textDocument = _editor.document;
 
             let invalidRange = new vscode.Range(
               0,
@@ -72,17 +89,14 @@ export function activate(context: vscode.ExtensionContext) {
               0
             );
             let fullRange = textDocument!.validateRange(invalidRange);
-            editor?.edit(edit => edit.replace(fullRange, message.text));
-
-            // vscode.window.showErrorMessage(message.text);
+            _editor.edit(edit => edit.replace(fullRange, message.text));
             return;
         }
       }, null);
 
-      const text = editor?.document.getText();
       panel.webview.postMessage({
         command: "text",
-        text: editor?.document.getText()
+        text: _editor.document.getText()
       });
     })
   );
@@ -104,4 +118,4 @@ export function activate(context: vscode.ExtensionContext) {
   }
 }
 
-export function deactivate() {}
+export function deactivate() { }
